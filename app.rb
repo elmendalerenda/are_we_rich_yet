1e68e65287a2b9995e3aeaa8185d582145806c78
Miguel Angel Fernandez <elmendalerenda@gmail.com>
2015-10-09 02:21:40 +0000

    add more fields to the form

--- app.rb
+++ app.rb
@@ -11,11 +11,14 @@ post '/spread' do
   agreement = OptionsAgreement.new(
     stock: (params['stocks']).to_f,
     strike_price: (params['strikeprice']).to_f,
-    grant_date: Date.strptime(params['grantdate'], '%m-%d-%Y'),
-    cliff_pct: 100.0,
-    cliff_n_months: 1
+    grant_date: Date.parse(params['grantdate']),
+    cliff_pct: params['cliff_pct'].to_f,
+    cliff_n_months: params['cliff_n_month'].to_f,
+    vesting_period: params['vesting_period'].to_f,
+    vesting_rate_after_cliff: :monthly
   )
-  spread = AWRY.spread(agreement, 30.0, Date.today)
+
+  spread = AWRY.spread(agreement, params['market_price'].to_f, Date.today)
 
   { spread: spread }.to_json
 end
--- public/js/main.js
+++ public/js/main.js
@@ -4,10 +4,15 @@ function submitForm() {
       {
         "stocks": $('#stocks').val(),
         "grantdate": $('#grantdate').val(),
-        "strikeprice": $('#strikeprice').val()
+        "strikeprice": $('#strikeprice').val(),
+        "cliff_pct": $('#cliff_pct').val(),
+        "cliff_n_months": $('#cliff_n_months').val(),
+        "vesting_period": $('#vesting_period').val(),
+        "market_price": $('#market_price').val()
       },
       function (result){
-        $('#spread').text('$ ' + Number(JSON.parse(result)['spread']).toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1,'))
+        $('#spread').text('$ ' + Number(JSON.parse(result)['spread']).toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1,'));
+        $('#spread').show();
       }
   );
 }
