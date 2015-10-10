b8e38ac5510d869d478d06f61af606c6cd290172
Miguel Angel Fernandez <elmendalerenda@gmail.com>
2015-10-10 05:02:08 +0000

    fix and clean tests

--- lib/options_agreement.rb
+++ lib/options_agreement.rb
@@ -3,7 +3,15 @@ require 'ostruct'
 class OptionsAgreement < OpenStruct
   def vested_portion(exercise_date)
     return 0.0 if exercise_date < (grant_date >> cliff_n_months)
-    1.0*(cliff_pct / 100.0)
+
+    cliff_release_portion = (cliff_pct / 100.0)
+
+    pct_per_month = (1.0 - cliff_release_portion) / vesting_period
+    cliff_ends = grant_date >> cliff_n_months
+
+    vested_months = exercise_date.month - cliff_ends.month + 12 * (exercise_date.year - cliff_ends.year)
+    pct = cliff_release_portion + (vested_months * pct_per_month)
+    pct
   end
 end
 
--- spec/are_we_rich_yet_spec.rb
+++ spec/are_we_rich_yet_spec.rb
@@ -7,11 +7,12 @@ describe 'AWRY calculates the spread' do
   context 'vested quantity is based on the grant date' do
     it 'spread is positive after the cliff' do
       agreement = OptionsAgreement.new(
-        stock: 5000.0,
+        stock: 5_000.0,
         strike_price: 10.0,
         grant_date: Date.new(2013, 1, 21),
         cliff_pct: 100.0,
-        cliff_n_months: 1
+        cliff_n_months: 1,
+        vesting_period: 24
       )
       date_after_cliff = Date.new(2013, 2, 21)
 
@@ -35,21 +36,6 @@ describe 'AWRY calculates the spread' do
       expect(spread).to eql(0.0)
     end
 
-    it 'the cliff releases a vested pct when it ends' do
-      agreement = OptionsAgreement.new(
-        stock: 5000.0,
-        strike_price: 10.0,
-        grant_date: Date.new(2013, 1, 21),
-        cliff_pct:30.0,
-        cliff_n_months: 12
-      )
-      date_after_cliff = Date.new(2015, 3, 21)
-
-      spread = AWRY.spread(agreement, market_price, date_after_cliff)
-
-      expect(spread).to eql(30_000.00)
-    end
-
     context 'vested pct is released within a schedule' do
       it 'stock is vested monthly after the cliff' do
         agreement = OptionsAgreement.new(
@@ -58,13 +44,13 @@ describe 'AWRY calculates the spread' do
           grant_date: Date.new(2013, 1, 21),
           cliff_pct: 30.0,
           cliff_n_months: 12,
-          vesting_period: 24,
-          vesting_rate_after_cliff: :monthly
+          vesting_period: 24
         )
+        date_2_months_after_cliff_ends = Date.new(2014, 3, 21)
 
-        spread = AWRY.spread(agreement, market_price, Date.new(2015, 3, 21))
+        spread = AWRY.spread(agreement, market_price, date_2_months_after_cliff_ends)
 
-        expect(spread).to eql(30_000.00)
+        expect(spread).to eql(35833.333333333336)
       end
     end
   end
